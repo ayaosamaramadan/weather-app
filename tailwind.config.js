53589f4216c33f0d1ca41761491eb7f0ed173a25
Aya Osama <ayaramadan2011@gmail.com>
2025-01-03 03:23:09 +0200

    modif styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,13 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   screen: {
-    'sm': '640px',
-    'md': '768px',
-    'lg': '1024px',
-    'xl': '1280px',
+    '2sm': '320px', // min-width: 320px
+      'sm': '375px', // min-width: 375px
+      'md': '425px', // min-width: 425px
+      'lg': '768px', // min-width: 768px
+      'xl': '1024px', // min-width: 1024px 
+      '2xl': '1440px', // min-width: 1440px
+    
   },
   theme: {
     extend: {},
